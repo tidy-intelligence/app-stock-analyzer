66324d2463f6a4a2e725a1cf88cce2f55b33a37d
Christoph Scheuch <19249734+christophscheuch@users.noreply.github.com>
2024-06-04 13:56:38 +0200

    Added portfolio weights computation and efficient frontier plot

--- app.R
+++ app.R
@@ -7,12 +7,15 @@ library(gt)
 library(gtExtras)
 library(svglite)
 library(scales)
+library(ggplot2)
 
 # TODO: add 52 week range
 # TODO: add average trading volume
 # TODO: add daily trading volume as bar charts
-# TODO: create second table with optimal weights
 # TODO: add option to switch indexes (high effort)
+# TODO: make sure that all boxes are centered and have same width
+# TODO: add figure description below figure
+# TODO: make figure interactive
 
 # Load data -----------------------------------------------------------
 
@@ -77,6 +80,37 @@ create_table_summary <- function(data) {
     ) 
 }
 
+create_table_weights <- function(portfolio_weights) {
+  portfolio_weights |> 
+    mutate(image = symbol) |> 
+    select(image, symbol, everything()) |> 
+    gt() |> 
+    text_transform(
+      locations = cells_body(columns = image),
+      fn = function(x) {
+        local_image(
+          filename = paste0("data/logos/", x, ".webp")
+        )
+      }
+    ) |> 
+    fmt_percent(
+      columns = c(mvp_weights, efp_weights),
+      decimals = 0
+    ) |> 
+    cols_label(image = "",
+               symbol = "", 
+               mvp_weights = "Minimum-Variance",
+               efp_weights = "Efficient Portfolio") |> 
+    tab_spanner(
+      label = "Portfolio Weights",
+      columns = c(mvp_weights, efp_weights)
+    ) |> 
+    tab_footnote(
+      footnote = "Efficient portfolio weights are computed using a benchmark multiple of 3.",
+      locations = cells_column_labels("efp_weights")
+    ) 
+}
+
 prepare_capm_data <- function(capm_data, input) {
   capm_data |> 
     filter(symbol %in% input$selected_symbols) |> 
@@ -104,6 +138,93 @@ prepare_stock_data <- function(stock_data, input) {
     mutate(
       image = symbol
     ) 
+}
+
+calculate_portfolio_weights <- function(stock_data, input) {
+  
+  # Create returns matrix
+  returns <- stock_data |> 
+    filter(symbol %in% input$selected_symbols) |> 
+    select(symbol, ret, date)
+  
+  returns_matrix <- returns |>
+    pivot_wider(
+      names_from = symbol,
+      values_from = ret
+    ) |>
+    select(-date)
+  sigma <- cov(returns_matrix)
+  mu <- colMeans(returns_matrix)
+  
+  # Minimum-variance portfolio weights
+  N <- ncol(returns_matrix)
+  iota <- rep(1, N)
+  sigma_inv <- solve(sigma)
+  mvp_weights <- sigma_inv %*% iota
+  mvp_weights <- mvp_weights / sum(mvp_weights)
+  tibble(
+    average_ret = as.numeric(t(mvp_weights) %*% mu),
+    volatility = as.numeric(sqrt(t(mvp_weights) %*% sigma %*% mvp_weights))
+  )
+  
+  # Efficient portfolio weights
+  benchmark_multiple <- 3
+  mu_bar <- benchmark_multiple * t(mvp_weights) %*% mu
+  C <- as.numeric(t(iota) %*% sigma_inv %*% iota)
+  D <- as.numeric(t(iota) %*% sigma_inv %*% mu)
+  E <- as.numeric(t(mu) %*% sigma_inv %*% mu)
+  lambda_tilde <- as.numeric(2 * (mu_bar - D / C) / (E - D^2 / C))
+  efp_weights <- mvp_weights +
+    lambda_tilde / 2 * (sigma_inv %*% mu - D * mvp_weights)
+  
+  tibble(
+    "symbol" = colnames(returns_matrix),
+    "mvp_weights" = as.numeric(mvp_weights),
+    "efp_weights" = as.numeric(efp_weights)
+  )
+}
+
+draw_efficient_frontier <- function(portfolio_weights) {
+  length_year <- 12
+  res <- tibble(
+    a = seq(from = -0.4, to = 1.9, by = 0.01),
+    mu = NA,
+    sd = NA
+  )
+  for (i in seq_along(a)) {
+    w <- (1 - a[i]) * portfolio_weights$mvp_weights + a[i] * portfolio_weights$efp_weights
+    res$mu[i] <- length_year * t(w) %*% mu   
+    res$sd[i] <- sqrt(length_year) * sqrt(t(w) %*% sigma %*% w)
+  }
+  
+  figure <- res |>
+    ggplot(aes(x = sd, y = mu)) +
+    geom_point() +
+    geom_point(
+      data = res |> filter(a %in% c(0, 1)),
+      size = 4
+    ) +
+    geom_point(
+      data = tibble(
+        mu = length_year * mu,       
+        sd = sqrt(length_year) * sqrt(diag(sigma))
+      ),
+      aes(y = mu, x = sd), size = 1
+    ) +
+    labs(
+      x = "Annualized standard deviation",
+      y = "Annualized expected return",
+      title = "Efficient frontier for selected stocks"
+    ) +
+    scale_x_continuous(labels = percent) +
+    scale_y_continuous(labels = percent) +
+    theme_classic() +
+    theme(panel.grid.minor = element_blank(),
+          panel.grid.major = element_blank(),
+          strip.background = element_blank(),
+          axis.ticks = element_blank())
+  
+  figure
 }
 
 # User interface ----------------------------------------------------------
@@ -137,12 +258,34 @@ ui <- fluidPage(
   fluidRow(
     box(
       width = 12,
-      withSpinner(gt_output("table_summary"))
+      withSpinner(
+        gt_output("table_summary")
+      )
+    )
+  ),
+
+  # Weights table
+  fluidRow(
+    box(
+      width = 12,
+      withSpinner(
+        gt_output("table_weights")
+      )
+    )
+  ),
+  
+  # Efficient frontier figure
+  fluidRow(
+    box(
+      width = 12,
+      withSpinner(
+        plotOutput("figure_frontier"),
+      )
     )
   )
 )
 
-# input <- list("selected_symbols" = c("MSFT", "MCD", "UNH", "AAPL"))
+# input <- list("selected_symbols" = c("MSFT", "NVDA", "UNH", "AAPL"))
 
 # Server ------------------------------------------------------------------
 server <- function(input, output, session) {
@@ -168,13 +311,31 @@ server <- function(input, output, session) {
     }
   })
   
+  portfolio_weights <- eventReactive(input$button, {
+    stock_data |> 
+      calculate_portfolio_weights(input) 
+  })
+  
   output$table_summary <- render_gt({
     if (is.null(stock_data_prepared())) {
       gt(tibble())
     } else {
-      stock_data_prepared() |> 
+      stock_data_prepared() |>
         create_table_summary()
     }
+  })
+
+  output$table_weights <- render_gt({
+    if (is.null(stock_data_prepared())) {
+      gt(tibble())
+    } else {
+      portfolio_weights() |>
+        create_table_weights()
+    }
+  })
+  
+  output$figure_frontier <- renderPlot({
+    draw_efficient_frontier(portfolio_weights())
   })
 }
 
